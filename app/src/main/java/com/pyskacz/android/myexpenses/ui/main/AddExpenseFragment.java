fc93c6552f5bee79232821642ac167596f3b0b60
Michal Babiarz <michal.babiarz84@gmail.com>
2020-02-09 11:25:10 +0100

    Fragments initial version

---
+++ app/src/main/java/com/pyskacz/android/myexpenses/ui/main/AddExpenseFragment.java
@@ -0,0 +1,51 @@
+package com.pyskacz.android.myexpenses.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.pyskacz.android.myexpenses.R;
+
+public class AddExpenseFragment extends Fragment {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+//    private PageViewModel pageViewModel;
+//
+////    public static AddExpenseFragment newInstance(int index) {
+//        AddExpenseFragment fragment = new AddExpenseFragment();
+//        Bundle bundle = new Bundle();
+//        bundle.putInt(ARG_SECTION_NUMBER, index);
+//        fragment.setArguments(bundle);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+//        int index = 1;
+//        if (getArguments() != null) {
+//            index = getArguments().getInt(ARG_SECTION_NUMBER);
+//        }
+//        pageViewModel.setIndex(index);
+//    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_add_expense, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        textView.setText("Dodać formularz");
+//        pageViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/pyskacz/android/myexpenses/ui/main/ExpenseListFragment.java
@@ -0,0 +1,39 @@
+package com.pyskacz.android.myexpenses.ui.main;
+
+import android.os.Bundle;
+import android.widget.SimpleAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.ListFragment;
+
+import com.pyskacz.android.myexpenses.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ExpenseListFragment extends ListFragment {
+
+    String[] text1 = {"left_1", "left_2", "left_3", "left_4", "left_5", "left_6"};
+    String[] text2 = {"right_1", "right_2", "right_3", "right_4", "right_5", "right_6"};
+
+    private static int counter = 0;
+
+    @Override
+    public void onActivityCreated(@NonNull Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        counter++;
+        ArrayList<HashMap<String, String>> expensesList = new ArrayList<>();
+        for (int i = 0; i < text1.length; i++) {
+            HashMap<String, String> keyToExpenseDetailsMapping = new HashMap<>();
+            keyToExpenseDetailsMapping.put("text1", text1[i] + "_" + counter);
+            keyToExpenseDetailsMapping.put("text2", text2[i] + "_" + counter);
+            expensesList.add(keyToExpenseDetailsMapping);
+        }
+
+        String[] keys = {"text1", "text2"};
+        int[] values = {R.id.expenseText1, R.id.expenseText2};
+
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), expensesList, R.layout.widget_expense_list_item, keys, values);
+        setListAdapter(adapter);
+    }
+}
